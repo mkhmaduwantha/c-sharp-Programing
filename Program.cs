bfa8b5be6415c62a5f2c05aef4130b21fc945523
mkhmaduwantha <mkhmaduwantha@gmail.com>
2023-03-10 12:18:34 +0530

    hello world in main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Main {
+    public static void Main(string[] args){
+        Console.WriteLine("Hello World");
+    }
+}
